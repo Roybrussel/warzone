83d241a2b44bebe51c46d54b5ff3c062431b7e58
Roy-Arne Brussel <29131401+Roybrussel@users.noreply.github.com>
2021-02-05 15:55:01 +0100

    Added React Bootstrap and a navbar

--- src/Components/Index/Index.js
+++ src/Components/Index/Index.js
@@ -1,10 +1,22 @@
 import React from "react";
 import "./Index.css";
 
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
+
 const Index = () => {
   return (
     <div>
-      <h1>TEST</h1>
+      <Navbar />
+      <div>
+        <h1>TEST</h1>
+        <h2>TEST</h2>
+        <h3>TEST</h3>
+        <h4>TEST</h4>
+        <h5>TEST</h5>
+        <h6>TEST</h6>
+      </div>
+      <Footer />
     </div>
   );
 };
